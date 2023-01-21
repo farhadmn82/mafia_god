e7443ea5b55df522b6d02af269af9d22ddd9aa20
farhadmn82 <66307849+farhadmn82@users.noreply.github.com>
2023-01-21 16:18:05 +0100

    logic completed

--- index.php
+++ index.php
@@ -4,7 +4,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="css/w3.css">
     <link rel="stylesheet" href="css/main.css" />
-    <script type="module" src="js/Player.js"></script>
     <script src="js/game.js"></script>
     <script src="js/jquery-2.0.3.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -28,6 +27,16 @@
             <divp id="night" class="frame frame_night"><?php include 'pages/night.html'; ?></divp>
             <divp id="sleep" class="frame frame_night"><?php include 'pages/sleep.html'; ?></divp>
             <divp id="nightAction" class="frame frame_night"><?php include 'pages/nightAction.html'; ?></divp>
+            <divp id="report" class="frame frame_night">
+            <div id="reportNightShootMessage" class="page_text"></div>
+                <button id="showInspctionButton" class="btn center btn_submit w3-amber" onclick="showreportinspection()">مشاهده استعلام جان سخت</button>
+                <div id="reportInspectionMessage" class="page_text"></div>
+                <button id="reportButton" class="btn center btn_submit" onClick="load_day()">ادامه</button>
+            </divp>
+            <divp id="end" class="frame frame_day">
+                <div id="endMessage" class="page_text"></div>
+                <button id="endButton" class="btn center btn_submit" onClick="load_start()">پایان بازی</button>
+            </divp>
         </div>
     </div>
 
--- js/game.js
+++ js/game.js
@@ -1,4 +1,3 @@
-
 //Initialization 
 const RoleTypes = {
 	None: 0,
@@ -32,10 +31,11 @@ class Role{
 }
 
 class Player {
-    constructor(name, roleId) {
-      this.name = name;
-      this.roleId = roleId;
-      this.status = 0; // 0: In Game, 1: Voted, 2: NightShoot, 3: KickedOut 
+    constructor(id, name, roleId) {
+        this.id = id
+        this.name = name;
+        this.roleId = roleId;
+        this.status = 0; // 0: In Game, 1: Voted, 2: NightShoot, 3: KickedOut 
     }
     
     isInGame(){
@@ -69,17 +69,29 @@ class Action{
     }
 }
 
+const RoleName = {
+    NoRole : 0,
+    GodFather : 1,
+    Mafia : 2,
+    Citizen : 3,
+    Doctor : 4,
+    Inspector : 5,
+    Snipper : 6,
+    DieHard : 7,
+    Guard : 8,
+}
+
 function Roles_Factory(){
     let roles = [
-        new Role("No Role", RoleTypes['None']), 
-        new Role("GodFather", RoleTypes['Mafia']), 
-        new Role("Mafia", RoleTypes['Mafia']), 
-        new Role("Citizen", RoleTypes['Citizen']), 
-        new Role("Doctor", RoleTypes['Citizen']), 
-        new Role("Inspector", RoleTypes['Citizen']), 
-        new Role("Snipper", RoleTypes['Citizen']), 
-        new Role("DieHard", RoleTypes['Citizen']), 
-        new Role("Security", RoleTypes['Citizen']), 
+        new Role(RoleName.NoRole, RoleTypes['None']), 
+        new Role(RoleName.GodFather, RoleTypes['Mafia']), 
+        new Role(RoleName.Mafia, RoleTypes['Mafia']), 
+        new Role(RoleName.Citizen, RoleTypes['Citizen']), 
+        new Role(RoleName.Doctor, RoleTypes['Citizen']), 
+        new Role(RoleName.Inspector, RoleTypes['Citizen']), 
+        new Role(RoleName.Snipper, RoleTypes['Citizen']), 
+        new Role(RoleName.DieHard, RoleTypes['Citizen']), 
+        new Role(RoleName.Guard, RoleTypes['Citizen']), 
     ];
 
     return roles;
@@ -87,73 +99,47 @@ function Roles_Factory(){
 
 function Players_Factory(){
     let players = [
-        new Player("A", 1), 
-        new Player("B", 5), 
-        new Player("فرهاد", 3),
-        new Player("C", 4) 
+        new Player(0, "A", 1), 
+        new Player(1, "B", 5), 
+        new Player(2, "فرهاد", 3),
+        new Player(3, "C", 4) 
     ];
 
     return players;
 }
 
-
-
 ///
 let ROLES = Roles_Factory();
 let players = Players_Factory();
 let nightActions = [];
-var STATUS = ['start', 'day', 'night', '']
 var gameStatus = 0;
 var numDayNight = 1;
-
+const DETAIL_REPORT = false;
+const DOCTOR_SELF_SAVE_MAX = 1
+const GODFATHER_NEGATIVE_INSPECTION_MAX = 1
 
 function show_page(page){
     $('divp').hide();
 
     $("#"+page).show();
-    
-}
-
-function load_voting(){
-    show_page("voting");
-
-    $("#votingPlayerList").html("");
-    for (i=0; i < players.length; i++){
-        if (players[i].isInGame())
-            $("#votingPlayerList").append('<button class="btn btn_player" onClick="votePlayer(' + i + ')">' + players[i].name + '</button>');
-        else
-            $("#votingPlayerList").append('<button disabled class="btn btn_player">' + players[i].name + '</button>')
-    }
-     
-    $("#votingPlayerList").append('<br/><button class="btn btn_player w3-indigo w3-center" onClick="votePlayer(-1)">'+ 'هیچکس' + '</button>');
-}
-
-function votePlayer(playerIndex){
-    if (playerIndex == -1){
-        if (confirm('هیچ بازیکنی خارج نشود و به شب برویم؟')) {
-            load_sleep();
-        }    
-    }
-    else{
-        if (confirm('آیا ' + "'" + players[playerIndex].name + "'" + ' در رای گیری خارج شود؟')) {
-            players[playerIndex].voted();
-            load_sleep();
-        }
-    }
-        
 }
 
 function load_start(){
     show_page("start");
 }
 
+// DAY
 function load_day(){
-    $("#page_title").html('روز ' + numDayNight);
-    show_page("day");
-}
-
-function speak_countdown_time(){
-
+    if (getMafiaCount() == 0){
+        load_end(true);
+    }
+    else if (getMafiaCount() >= getCitizenCount()){
+        load_end(false);
+    }
+    else{
+        $("#page_title").html('روز ' + numDayNight);
+        show_page("day");    
+    }
 }
 
 var start_time;
@@ -192,6 +178,63 @@ function pause_countdown_timer(){
         restart_countdown_timer(remain);
     }
         
+}
+
+// VOTING
+function load_voting(){
+    show_page("voting");
+
+    $("#votingPlayerList").html("");
+    for (i=0; i < players.length; i++){
+        if (players[i].isInGame())
+            $("#votingPlayerList").append('<button class="btn btn_player" onClick="votePlayer(' + i + ')">' + players[i].name + '</button>');
+        else
+            $("#votingPlayerList").append('<button disabled class="btn btn_player">' + players[i].name + '</button>')
+    }
+     
+    $("#votingPlayerList").append('<br/><button class="btn btn_player w3-indigo w3-center" onClick="votePlayer(-1)">'+ 'هیچکس' + '</button>');
+}
+
+function votePlayer(playerIndex){
+    if (playerIndex == -1){
+        if (confirm('هیچ بازیکنی خارج نشود و به شب برویم؟')) {
+            load_sleep();
+        }    
+    }
+    else{
+        if (confirm('آیا ' + "'" + players[playerIndex].name + "'" + ' در رای گیری خارج شود؟')) {
+            players[playerIndex].voted();
+            if (getMafiaCount() == 0){
+                load_end(true);
+            }
+            else if (getMafiaCount() >= getCitizenCount()){
+                load_end(false);
+            }
+            else{
+                load_sleep();
+            }
+        }
+    }
+}
+
+function getMafiaCount(){
+    var count = 0;
+    for (i=0; i < players.length; i++){
+        if (players[i].isInGame() && ROLES[players[i].roleId].typeId == RoleTypes.Mafia)
+            count++;
+    }
+
+    return count;
+}
+
+function getCitizenCount(){
+    var count = 0;
+    for (i=0; i < players.length; i++){
+        if (players[i].isInGame() && ROLES[players[i].roleId].typeId == RoleTypes.Citizen)
+            count++;
+    }
+
+    return count;
 }
 
 // Sleep
@@ -236,7 +279,7 @@ function load_night(){
     $("#actionPlayersList").html("");
     for (i=0; i < players.length; i++){
         if (players[i].isInGame()){
-            if(!isNightActionDone(players[i]))
+            if(!isNightActionDone(i))
                 $("#actionPlayersList").append('<button class="btn btn_player" onClick="showPlayerNightAction(' + i + ')">'+ players[i].name + '</button>');  
             else 
                 $("#actionPlayersList").append('<button disabled class="btn btn_player">'+ players[i].name + '</button>');  
@@ -249,28 +292,30 @@ function clearNightActionPage(){
     $("#nightActionArea").html("");
 }
 
-function isNightActionDone(player){
+function isNightActionDone(playerIndex){
     var done = false;
     nightActions.forEach(act => {
-        if (act.numNight == numDayNight && player.name == act.rolePlayer.name)
+        if (act.numNight == numDayNight && act.rolePlayer == playerIndex)
             done = true;
     });
     
     return done;
 }
 
-
 // Night Actions
 function showPlayerNightAction(playerIndex){
     if (confirm('آیا ' + "'" + players[playerIndex].name + "'" +   ' هستی؟')) {
         show_page('nightAction');
         switch (players[playerIndex].roleId){
-            case 0: no_action(playerIndex); break;
-            case 1: godFather_nightShoot(playerIndex); break;
-            case 2: no_action(playerIndex); break;
-            case 3: no_action(playerIndex); break;
-            case 4: doctor_save(playerIndex); break;
-            case 5: inspector_inspection(playerIndex); break;
+            case RoleName.NoRole: no_action(playerIndex); break;
+            case RoleName.GodFather: godFather_nightShoot(playerIndex); break;
+            case RoleName.Mafia: no_action(playerIndex); break;
+            case RoleName.Citizen: no_action(playerIndex); break;
+            case RoleName.Doctor: doctor_save(playerIndex); break;
+            case RoleName.Inspector: inspector_inspection(playerIndex); break;
+            case RoleName.DieHard: no_action(playerIndex); break;
+            case RoleName.Snipper: no_action(playerIndex); break;
+            case RoleName.Guard: no_action(playerIndex); break;
             default: no_role_error(playerIndex);
         }
     }  
@@ -284,7 +329,7 @@ function no_action(playerIndex){
     $("#nightActionArea").html("");
     for (i=0; i < players.length; i++){
         if (players[i].isInGame())
-            $("#nightActionArea").append('<button class="btn btn_player" onClick="submitNightAction(0,' + playerIndex + ',' + i + ')">' + players[i].name + '</button>');
+            $("#nightActionArea").append('<button class="btn btn_action btn_player" onClick="submitNightAction(0,' + playerIndex + ',' + i + ')">' + players[i].name + '</button>');
     }
 }
 
@@ -293,7 +338,7 @@ function godFather_nightShoot(godFatherId){
     $("#nightActionArea").html("");
     for (i=0; i < players.length; i++){
         if (players[i].isInGame() && ROLES[players[i].roleId].isCitizen())
-            $("#nightActionArea").append('<button class="btn btn_player" onClick="submitNightAction(1,' + godFatherId + ',' + i + ')">' + players[i].name + '</button>');
+            $("#nightActionArea").append('<button class="btn btn_action btn_player" onClick="submitNightAction(1,' + godFatherId + ',' + i + ')">' + players[i].name + '</button>');
     }
 }
 
@@ -304,12 +349,12 @@ function doctor_save(doctorIndex){
         if (i == doctorIndex && !doctorSelfSaveIsAllowed())
             continue;
         if (players[i].isInGame())
-            $("#nightActionArea").append('<button class="btn btn_player" onClick="submitNightAction(2,' + doctorIndex + ',' + i + ')">' + players[i].name + '</button>');
+            $("#nightActionArea").append('<button class="btn btn_action btn_player" onClick="submitNightAction(2,' + doctorIndex + ',' + i + ')">' + players[i].name + '</button>');
     }
 }
 
 var doctorSelfSave = 0;
-const DOCTOR_SELF_SAVE_MAX = 1
+
 function doctorSelfSaveIsAllowed(){
     if (doctorSelfSave < DOCTOR_SELF_SAVE_MAX)
         return true;
@@ -322,18 +367,17 @@ function inspector_inspection(inspectorIndex){
     $("#nightActionArea").html("");
     for (i=0; i < players.length; i++){
         if (players[i].isInGame())
-            $("#nightActionArea").append('<button class="btn btn_player" onClick="submitNightAction(3,' + inspectorIndex + ',' + i + ')">' + players[i].name + '</button>');
+            $("#nightActionArea").append('<button class="btn btn_action btn_player" onClick="submitNightAction(3,' + inspectorIndex + ',' + i + ')">' + players[i].name + '</button>');
     }
 }
 
-
 function submitNightAction(actionType, rolePlayerIndex, playerIndex){
-    nightActions.push(new Action(numDayNight, actionType, players[rolePlayerIndex], players[playerIndex]));
+    nightActions.push(new Action(numDayNight, actionType, rolePlayerIndex, playerIndex));
     if (actionType == ActionType.Inspected){
         var inps = "منفی";
         if(ROLES[players[playerIndex].roleId].isMafia())
         var inps = "مثبت";
-        if(ROLES[players[playerIndex].roleId].roleName == 'GodFather' && !godFatherInspectionResult())
+        if(ROLES[players[playerIndex].roleId].roleName == RoleName.GodFather && !godFatherInspectionResult())
         inps = 'منفی';
         
         $('#nightActionMessage').html('جواب استعلام ' + "'" + players[playerIndex].name + "'" + '<br/><strog>' + inps + '</strong>');
@@ -342,10 +386,7 @@ function submitNightAction(actionType, rolePlayerIndex, playerIndex){
     $("#nightActionArea").html('<button class="btn btn_submit" onClick="load_night()">اتمام</button>');    
 }
 
-
-
 var godFatherNegativeInspection = 0;
-const GODFATHER_NEGATIVE_INSPECTION_MAX = 1
 function godFatherInspectionResult(){
     if (godFatherNegativeInspection < GODFATHER_NEGATIVE_INSPECTION_MAX)
         return false;
@@ -353,39 +394,91 @@ function godFatherInspectionResult(){
         return true;
 }
 
-
 function start_next_day(){
+    doNightActions();
     numDayNight++;
-    //voice
-    //create report
-    //show report
-    //do changes
-    load_day();
+    play(1);
+    load_report();
 }
 
-function getPlayer(name){
-    var pl = undefined;
-    players.forEach(player => {
-        if (player.name == name)
-            pl = player;
+function load_report(){
+    $('#reportInspectionMessage').css("display", "none");
+    show_page('report');
+}
+
+function doNightActions(){   
+    var rep_dieHard_inspection = false;
+    var rep_night_shoot = -1;
+    var rep_doctor_save = -1;
+
+    nightActions.forEach(act => {
+        if (act.numNight == numDayNight){
+            
+            switch (players[act.rolePlayer].roleId){
+                case RoleName.NoRole: break;
+                case RoleName.GodFather: rep_night_shoot = act.player; break;
+                case RoleName.Mafia: break;
+                case RoleName.Citizen: break;
+                case RoleName.Doctor: rep_doctor_save = act.player; break;
+                case RoleName.Inspector: break;
+                case RoleName.DieHard: rep_dieHard_inspection = (act.player == 1); break;
+                case RoleName.Snipper: break;
+                case RoleName.Guard: break;
+                default: break;
+            }
+        }
     });
 
-    return pl;
-}
-
-
-function changeGameStatus(status){
-    switch (status){
-        case 'start': load_start(); break;
-        case 'voting': load_voting(); break;
-        case 'day': load_day(); break;
-        case 'night': break;
-        case 'sleep': load_sllep(); break;
-        case 'mafiaWakeUp': break;
-        case 'nightReport': break;
-        
+    var nightMsg = 'در شبی که گذشت هیچکس کشته نشد';
+    if (rep_doctor_save != rep_night_shoot){
+        players[rep_night_shoot].nightShoot();
+        nightMsg = 'کشته شب: ' + players[rep_night_shoot].name;
     }
+    $('#reportNightShootMessage').html(nightMsg);    
+    
+    var rep_mafia_out = 0;
+    var rep_citizen_out = 0;
+    var detailrep = '';
+    var repInsp = '';
+    $('#reportInspectionMessage').css("display", "none");
+    $('#showInspctionButton').css("display", "none");
+    if (rep_dieHard_inspection){
+        $('#showInspctionButton').css("display", "block");
+        for (i=0; i < players.length; i++){
+            if (!players[i].isInGame()){
+                ROLES[players[i].roleId] == RoleTypes.Mafia ? rep_mafia_out++ : rep_citizen_out++;
+                var belowAbove = Math.floor((Math.random() * 10));
+                if (belowAbove > 5)
+                    detailrep += '<br/>' + players[i].name;
+                else
+                    detailrep = '<br/>' + players[i].name + detailrep;
+            }
+        }
+        repInsp = 'گزارش بازیکنان بیرون از بازی' + '<br/>' + 'مافیا: ' + rep_mafia_out + '<br/>' + 'شهروند: ' + rep_citizen_out;
+    }
+
+    
+    if (DETAIL_REPORT){                
+        $('#reportInspectionMessage').html(repInsp + detailrep);
+    }
+
+    $('#reportInspectionMessage').html(repInsp);
 }
+
+function showreportinspection(){
+    $('#reportInspectionMessage').css("display", "block");
+}
+
+function load_end(citizenWin)
+{
+    show_page('end');
+    if(citizenWin){
+        $('#endMessage').html('شهروندها برنده شدند!');
+    }
+    else{
+        $('#endMessage').html('مافیا برنده شد!');
+    }
+} 
 
 function play() {
     var audio = new Audio('http://psychic3d.free.fr/extra_fichiers/sons/alerte3.wav');
@@ -394,6 +487,6 @@ function play() {
 
 function play(voiceId) {
     var audio = new Audio('audio\\' + voiceId + '.wav');
-    //audio.play();
+    audio.play();
 }
-const unit = 100;
+const unit = 1000;
