9f907e8019c7ebe74149409ef52b16dfb181df89
farhadmn82 <66307849+farhadmn82@users.noreply.github.com>
2023-01-21 10:03:28 +0100

    day completed

--- index.php
+++ index.php
@@ -7,32 +7,35 @@
     <script type="module" src="js/Player.js"></script>
     <script src="js/game.js"></script>
     <script src="js/jquery-2.0.3.min.js"></script>
-    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
     <div id='msg'></div>
-    <div id='nav_bar' class='navbar'style="height:6vh">
-        <button class="w3-button w3-circle w3-indigo w3-margin-left w3-margin-top"><</button>
+    <div id='nav_bar' class='navbar'>
+        <i class="fa fa-arrow-left w3-circle w3-xlarge w3-margin-left" style='margin-top:0.7vh;'></i>
     </div>
-    <div id='page_header' class='w3-center page_header w3-center'>
-        <div id='page_title' class='w3-center page_title w3-center'>MAFIA</div>
+    <div id='page_header' class='w3-container page_header'>
+        <div id='page_title' class='w3-center page_title w3-center'>
+            MAFIA
+        </div>
     
     </div>
 
-    <div class="w3-container" style="height:80vh">
-
-    <div class='page_content' style="height:100%">
-        <divp id="start"><?php include 'pages/start.html'; ?></divp>
-        <divp id="voting"><?php include 'pages/voting.html'; ?></divp>
-        <divp id="day"><?php include 'pages/day.html'; ?></divp>
-        <divp id="night"><?php include 'pages/night.html'; ?></divp>
-        <divp id="sleep"><?php include 'pages/sleep.html'; ?></divp>
-        <divp id="nightAction"><?php include 'pages/nightAction.html'; ?></divp>
+    <div class="w3-container content">
+        <div class='page_content'>
+            <divp id="start" class="frame_day"><?php include 'pages/start.html'; ?></divp>
+            <divp id="voting" class="frame_day"><?php include 'pages/voting.html'; ?></divp>
+            <divp id="day" class="frame_day"><?php include 'pages/day.html'; ?></divp>
+            <divp id="night" class="frame_night"><?php include 'pages/night.html'; ?></divp>
+            <divp id="sleep" class="frame_night"><?php include 'pages/sleep.html'; ?></divp>
+            <divp id="nightAction" class="frame_night"><?php include 'pages/nightAction.html'; ?></divp>
+        </div>
     </div>
+
+
+    <div class="w3-container footer">
+
     </div>
-
-
-
 
 
 <script>
